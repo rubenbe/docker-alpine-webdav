ec2ec394c123f163c41e60e2fb11e091adcbe1b9
Ruben Vandamme <github-com-00ff86@vandamme.email>
2017-12-09 19:18:05 +0100

    Make run.sh work with and without username

--- no_auth/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Alpine 3.4 still uses the old name 'httpd', instead of 'apache2'.
-httpd -D FOREGROUND
-
--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -eou pipefail
 
-htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
-chown root:apache /etc/apache2/webdav.password
-chmod 640 /etc/apache2/webdav.password
+if [ -v "USERNAME" -a -v "PASSWORD" ]; then
+  echo "Configuring user $USERNAME"
+  htpasswd -cb /etc/apache2/webdav.password "$USERNAME" "$PASSWORD"
+  chown root:apache /etc/apache2/webdav.password
+  chmod 640 /etc/apache2/webdav.password
+fi
 
 # Alpine 3.4 still uses the old name 'httpd', instead of 'apache2'.
 httpd -D FOREGROUND
